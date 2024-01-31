528aba960ddd59409fd0410a37677bdef6f5d2a7
Ibrokhim3 <ishamsiddinov.97@gmail.com>
2024-01-31 17:48:28 +0500

    webp ext added

--- src/js/modules/modal-sphere.js
+++ src/js/modules/modal-sphere.js
@@ -12,11 +12,23 @@ const modalSphereJs = () => {
 
   let viewerInstance = null;
 
+  function canUseWebP() {
+    var elem = document.createElement("canvas");
+    if (!!(elem.getContext && elem.getContext("2d"))) {
+      // was able or not to get WebP representation
+      return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
+    }
+    // very old browser like IE 8, canvas not supported
+    return false;
+  }
+
+  const supportWebp = canUseWebP();
+
   const selectThePhoto = (id) => {
-    if (id === "m1") return "../img/1.jpg";
-    if (id === "p1") return "../img/2.jpg";
-    if (id === "m2") return "../img/3.jpg";
-    if (id === "r2") return "../img/4.jpg";
+    if (id === "m1") return supportWebp ? "../img/1.webp" : "../img/1.jpg";
+    if (id === "p1") return supportWebp ? "../img/2.webp" : "../img/2.jpg";
+    if (id === "m2") return supportWebp ? "../img/3.webp" : "../img/3.jpg";
+    if (id === "r2") return supportWebp ? "../img/4.webp" : "../img/4.jpg";
   };
 
   //fixes url path
